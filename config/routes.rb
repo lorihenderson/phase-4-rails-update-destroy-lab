837d40215e645dac4c76c079218d3a4918ba4783
Lori Henderson <lorim95@gmail.com>
2022-07-20 17:11:03 -0400

    commit tree 53721b3d064ee25b7e9a086081f8e7dfe6f6dc5e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  resources :plants, only: [:index, :show, :create, :destroy]
+
+  # PATH update /plants/:id
+  patch '/plants/:id', to: 'plants#update'
 end
